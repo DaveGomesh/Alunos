a69db76e43f180ccee5e1e973e9b12ecf7aa37a1
David Gomesh <davidgomesh2016@gmail.com>
2022-11-07 00:44:56 -0300

    Removido cadastro ao iniciar aplicação

--- src/main/java/com/robotica/CartoesAcesso/runner/Run.java
+++ src/main/java/com/robotica/CartoesAcesso/runner/Run.java
@@ -1,18 +1,7 @@
 package com.robotica.CartoesAcesso.runner;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import com.robotica.CartoesAcesso.api.dto.mapper.AlunoMapper;
-import com.robotica.CartoesAcesso.api.dto.request.AlunoRequest;
-import com.robotica.CartoesAcesso.api.dto.request.CartaoRequest;
-import com.robotica.CartoesAcesso.domain.model.Aluno;
-import com.robotica.CartoesAcesso.domain.model.Curso;
-import com.robotica.CartoesAcesso.domain.service.AlunoService;
-import com.robotica.CartoesAcesso.domain.service.CursoService;
 
 import lombok.AllArgsConstructor;
 
@@ -20,25 +9,25 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class Run implements CommandLineRunner {
 
-    private final AlunoMapper alunoMapper;
+    // private final AlunoMapper alunoMapper;
 
-    private final CursoService cursoService;
-    private final AlunoService alunoService;
+    // private final CursoService cursoService;
+    // private final AlunoService alunoService;
 
     @Override
     public void run(String... args) throws Exception {
 
-        List<Curso> cursos = Arrays.asList(
-            Curso.builder().nome("Libras").build(),
-            Curso.builder().nome("Robótica").build(),
-            Curso.builder().nome("Sistemas de Informação").build(),
-            Curso.builder().nome("Informática").build(),
-            Curso.builder().nome("Edificações").build(),
-            Curso.builder().nome("Eletrônica").build(),
-            Curso.builder().nome("Eletromecânica").build(),
-            Curso.builder().nome("Eletrotécnica").build()
-        );
-        cursos.forEach(cursoService::salvar);
+        // List<Curso> cursos = Arrays.asList(
+        //     Curso.builder().nome("Libras").build(),
+        //     Curso.builder().nome("Robótica").build(),
+        //     Curso.builder().nome("Sistemas de Informação").build(),
+        //     Curso.builder().nome("Informática").build(),
+        //     Curso.builder().nome("Edificações").build(),
+        //     Curso.builder().nome("Eletrônica").build(),
+        //     Curso.builder().nome("Eletromecânica").build(),
+        //     Curso.builder().nome("Eletrotécnica").build()
+        // );
+        // cursos.forEach(cursoService::salvar);
 
         // AlunoRequest alunoRequest = (AlunoRequest.builder()
         //     .nome("David Gomesh")
